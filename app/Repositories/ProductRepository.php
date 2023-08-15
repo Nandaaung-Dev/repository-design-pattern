4ea030e2133b53b1d8970531b15aef3cb4b540d0
Nandaaung <nandaaung1689@gmail.com>
2023-08-15 16:35:59 +0630

    first commit

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function getAllProducts()
+    {
+        return Product::all();
+    }
+
+    public function getProductById($productId)
+    {
+        if (!$productId) return false;
+        return Product::findOrFail($productId);
+    }
+
+    public function deleteProduct($productId)
+    {
+        Product::destroy($productId);
+    }
+
+    public function createProduct(array $productData)
+    {
+        return Product::create($productData);
+    }
+
+    public function updateProduct($productId, array $productData)
+    {
+        return Product::whereId($productId)->update($productData);
+    }
+
+    public function getAvailableProducts()
+    {
+        return Product::where('is_available', true);
+    }
+}
